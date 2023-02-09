c7421b071c7703a9d6bbea07c146f10906dd9716
Athmane Benarous <atho5850@gmail.com>
2023-02-08 22:51:13 -0500

    Added findAppUserById in AppUser dao

--- backend/src/main/java/ca/mcgill/purposeful/dao/AppUserRepository.java
+++ backend/src/main/java/ca/mcgill/purposeful/dao/AppUserRepository.java
@@ -1,14 +1,15 @@
 package ca.mcgill.purposeful.dao;
 
-import org.springframework.data.repository.CrudRepository;
-
 import ca.mcgill.purposeful.model.AppUser;
+import org.springframework.data.repository.CrudRepository;
 
 /**
  * Repository for AppUser
  */
 
 public interface AppUserRepository extends CrudRepository<AppUser, Integer> {
+
+  AppUser findAppUserById(String id);
 
   AppUser findAppUserByUsername(String username);
 
