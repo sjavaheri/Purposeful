354ba471c6b2386f0e9d7f89c51609b1b873a49a
Athmane Benarous <atho5850@gmail.com>
2023-02-08 20:41:47 -0500

    Models for Idea, Reaction, Technology, URL, VerificationRequest

---
+++ backend/src/main/java/ca/mcgill/purposeful/model/Reaction.java
@@ -0,0 +1,81 @@
+package ca.mcgill.purposeful.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.Date;
+//import java.sql.Date;
+//import java.sql.Time;
+
+/**
+ * The Technology class, the model for all Technologies in the database
+ */
+@Entity
+public class Reaction {
+
+  // ------------------------
+  // Enumerations
+  // ------------------------
+
+  public enum ReactionType {HighFive}
+
+  // ------------------------
+  // Idea Attributes
+  // ------------------------
+
+  private String id;
+
+  private ReactionType reactionType;
+
+  private Date date;
+
+  //private Time time;
+
+  private String name;
+
+  // ------------------------
+  // Idea Constructor
+  // ------------------------
+
+  public Reaction() {
+
+  }
+
+  // ------------------------
+  // Getter/Setter Methods
+  // ------------------------
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public ReactionType getReactionType() {
+    return reactionType;
+  }
+
+  public void setReactionType(ReactionType reactionType) {
+    this.reactionType = reactionType;
+  }
+
+  public Date getDate() {
+    return date;
+  }
+
+  public void setDate(Date date) {
+    this.date = date;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
---
+++ backend/src/main/java/ca/mcgill/purposeful/model/VerificationRequest.java
@@ -0,0 +1,84 @@
+package ca.mcgill.purposeful.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+/**
+ * The Technology class, the model for all Technologies in the database
+ */
+@Entity
+public class VerificationRequest {
+
+  // ------------------------
+  // Enumerations
+  // ------------------------
+
+  public enum Status {Pending, Approved, Refused}
+
+  // ------------------------
+  // VerificationRequest Attributes
+  // ------------------------
+
+  private String companyOuiNumber;
+
+  private String companyName;
+
+  private String explanation;
+
+  private String supportingDocumentUrl;
+
+  private Status status;
+
+  // ------------------------
+  // VerificationRequest Constructor
+  // ------------------------
+
+  public VerificationRequest() {
+
+  }
+
+  // ------------------------
+  // Getter/Setter Methods
+  // ------------------------
+  @Id
+
+  public String getCompanyOuiNumber() {
+    return companyOuiNumber;
+  }
+
+  public void setCompanyOuiNumber(String companyOuiNumber) {
+    this.companyOuiNumber = companyOuiNumber;
+  }
+
+  public String getCompanyName() {
+    return companyName;
+  }
+
+  public void setCompanyName(String companyName) {
+    this.companyName = companyName;
+  }
+
+  public String getExplanation() {
+    return explanation;
+  }
+
+  public void setExplanation(String explanation) {
+    this.explanation = explanation;
+  }
+
+  public String getSupportingDocumentUrl() {
+    return supportingDocumentUrl;
+  }
+
+  public void setSupportingDocumentUrl(String supportingDocumentUrl) {
+    this.supportingDocumentUrl = supportingDocumentUrl;
+  }
+
+  public Status getStatus() {
+    return status;
+  }
+
+  public void setStatus(Status status) {
+    this.status = status;
+  }
+}
