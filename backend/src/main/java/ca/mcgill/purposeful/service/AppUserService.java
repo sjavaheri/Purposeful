34e827da1a70f6997da02e0fb4774582683e2b1a
Siger Ma <--local>
2023-02-12 18:57:46 -0500

    T033 Service method to create a new regular user

--- backend/src/main/java/ca/mcgill/purposeful/service/AppUserService.java
+++ backend/src/main/java/ca/mcgill/purposeful/service/AppUserService.java
@@ -1,13 +1,17 @@
 package ca.mcgill.purposeful.service;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import ca.mcgill.purposeful.configuration.Authority;
 import ca.mcgill.purposeful.dao.AppUserRepository;
+import ca.mcgill.purposeful.dao.RegularUserRepository;
 import ca.mcgill.purposeful.model.AppUser;
+import ca.mcgill.purposeful.model.RegularUser;
 import ca.mcgill.purposeful.model.SecurityUser;
 
 @Service
@@ -15,6 +19,69 @@ public class AppUserService implements UserDetailsService {
 
   @Autowired
   AppUserRepository appUserRepository;
+
+  @Autowired
+  RegularUserRepository regularUserRepository;
+
+  /**
+   * Register a new regular user
+   * 
+   * @param email    - email of the user
+   * @param username - username of the user
+   * @param password - password of the user
+   * @return AppUser - the newly created user
+   * @author Siger Ma
+   */
+  @Transactional
+  public AppUser registerRegularUser(String email, String username, String password) {
+
+    // Error validation
+    if (email == null || email.isEmpty()) {
+      throw new IllegalArgumentException("Please enter a valid email. Email cannot be left empty");
+    }
+    if (username == null || username.isEmpty()) {
+      throw new IllegalArgumentException("Please enter a valid username. Username cannot be left empty");
+    }
+    if (password == null || password.isEmpty()) {
+      throw new IllegalArgumentException("Please enter a valid password. Password cannot be left empty");
+    }
+    if (!email.matches("^[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$")) {
+      throw new IllegalArgumentException("Please enter a valid email. The email address you entered is not valid");
+    }
+    if (password.length() < 8 || !password.matches(".*[0-9].*") || !password.matches(".*[A-Z].*")
+        || !password.matches(".*[a-z].*")) {
+      throw new IllegalArgumentException(
+          "Please enter a valid password. Passwords must be at least 8 characters long and contain at least one number, one lowercase character and one uppercase character");
+    }
+    if (appUserRepository.findAppUserByEmail(email) != null) {
+      throw new IllegalArgumentException("An account with this email address already exists");
+    }
+    if (appUserRepository.findAppUserByUsername(username) != null) {
+      throw new IllegalArgumentException("An account with this username already exists");
+    }
+
+    AppUser appUser = null;
+
+    try {
+      // Create app user
+      appUser = new AppUser();
+      appUser.setEmail(email);
+      appUser.setUsername(username);
+      appUser.setPassword(password);
+      appUser.getAuthorities().add(Authority.User);
+      appUserRepository.save(appUser);
+
+      // Create role
+      RegularUser regularUser = new RegularUser();
+      regularUser.setAppUser(appUser);
+      regularUser.setVerifiedCompany(false);
+      regularUserRepository.save(regularUser);
+    } catch (Exception e) {
+      throw new RuntimeException("An error occurred while creating your account. Please try again later");
+    }
+
+    return appUser;
+  }
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
