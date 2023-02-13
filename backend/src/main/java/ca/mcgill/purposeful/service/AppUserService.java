15568ad61c2c3bdd398bacfe404d96977288fe6a
Siger Ma <--local>
2023-02-12 23:21:37 -0500

    T033 Correct Exception handler

--- backend/src/main/java/ca/mcgill/purposeful/service/AppUserService.java
+++ backend/src/main/java/ca/mcgill/purposeful/service/AppUserService.java
@@ -2,6 +2,7 @@ package ca.mcgill.purposeful.service;
 
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import ca.mcgill.purposeful.configuration.Authority;
 import ca.mcgill.purposeful.dao.AppUserRepository;
 import ca.mcgill.purposeful.dao.RegularUserRepository;
+import ca.mcgill.purposeful.exception.GlobalException;
 import ca.mcgill.purposeful.model.AppUser;
 import ca.mcgill.purposeful.model.RegularUser;
 import ca.mcgill.purposeful.model.SecurityUser;
@@ -40,30 +42,28 @@ public class AppUserService implements UserDetailsService {
 
     // Error validation
     if (email == null || email.isEmpty()) {
-      throw new IllegalArgumentException("Please enter a valid email. Email cannot be left empty");
+      throw new GlobalException(HttpStatus.BAD_REQUEST, "Please enter a valid email. Email cannot be left empty");
     }
     if (username == null || username.isEmpty()) {
-      throw new IllegalArgumentException(
-          "Please enter a valid username. Username cannot be left empty");
+      throw new GlobalException(HttpStatus.BAD_REQUEST, "Please enter a valid username. Username cannot be left empty");
     }
     if (password == null || password.isEmpty()) {
-      throw new IllegalArgumentException(
-          "Please enter a valid password. Password cannot be left empty");
+      throw new GlobalException(HttpStatus.BAD_REQUEST, "Please enter a valid password. Password cannot be left empty");
     }
     if (!email.matches("^[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$")) {
-      throw new IllegalArgumentException(
+      throw new GlobalException(HttpStatus.BAD_REQUEST,
           "Please enter a valid email. The email address you entered is not valid");
     }
     if (password.length() < 8 || !password.matches(".*[0-9].*") || !password.matches(".*[A-Z].*")
         || !password.matches(".*[a-z].*")) {
-      throw new IllegalArgumentException(
+      throw new GlobalException(HttpStatus.BAD_REQUEST,
           "Please enter a valid password. Passwords must be at least 8 characters long and contain at least one number, one lowercase character and one uppercase character");
     }
     if (appUserRepository.findAppUserByEmail(email) != null) {
-      throw new IllegalArgumentException("An account with this email address already exists");
+      throw new GlobalException(HttpStatus.BAD_REQUEST, "An account with this email address already exists");
     }
     if (appUserRepository.findAppUserByUsername(username) != null) {
-      throw new IllegalArgumentException("An account with this username already exists");
+      throw new GlobalException(HttpStatus.BAD_REQUEST, "An account with this username already exists");
     }
 
     AppUser appUser = null;
@@ -83,7 +83,7 @@ public class AppUserService implements UserDetailsService {
       regularUser.setVerifiedCompany(false);
       regularUserRepository.save(regularUser);
     } catch (Exception e) {
-      throw new RuntimeException(
+      throw new GlobalException(HttpStatus.INTERNAL_SERVER_ERROR,
           "An error occurred while creating your account. Please try again later");
     }
 
