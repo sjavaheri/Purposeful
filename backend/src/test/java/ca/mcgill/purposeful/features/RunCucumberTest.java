eaa2b1dfbda20978e248947ed6e8dc2ecac15cf8
Shidan Javaheri <97904673+sjavaheri@users.noreply.github.com>
2023-02-13 12:23:06 -0500

    #45 Integrated cucumber tests to gradle. Feature files need to be refined

---
+++ backend/src/test/java/ca/mcgill/purposeful/features/RunCucumberTest.java
@@ -0,0 +1,16 @@
+package ca.mcgill.purposeful.features;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+// @RunWith(Cucumber.class)
+
+// @CucumberOptions(
+//   plugin = {"pretty", "html:target/cucumber-report.html"},
+//   features = {"src/test/resources"}
+// )
+// public class RunCucumberTest {
+// }
+
