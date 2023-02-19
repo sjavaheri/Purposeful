d2975cea62959769538be86dc07e181b2351cd0d
Thibaut Baguette <thibaut.baguette@mail.mcgill.ca>
2023-02-19 13:07:50 -0500

    Create getIdeaDetailsById controller method

--- backend/src/main/java/ca/mcgill/purposeful/controller/IdeaController.java
+++ backend/src/main/java/ca/mcgill/purposeful/controller/IdeaController.java
@@ -1,22 +1,23 @@
 package ca.mcgill.purposeful.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import ca.mcgill.purposeful.exception.GlobalException;
+import ca.mcgill.purposeful.dto.DetailedIdeaDTO;
 import ca.mcgill.purposeful.model.Idea;
+import ca.mcgill.purposeful.service.IdeaService;
 
 /**
  * API for demonstrating how permissions work for access to endpoints
  */
-
 @RestController
-@RequestMapping({"/idea", "/idea/"})
+@RequestMapping({ "/idea", "/idea/" })
 public class IdeaController {
+    @Autowired
+    IdeaService ideaService;
 
     /**
      * This methods gets all librarians for the database
@@ -25,8 +26,16 @@ public class IdeaController {
      * @return update idea
      * @throws Exception
      */
-//   @PutMapping
-//   public IdeaDTO modifyIdea(@RequestParam String title, @RequestParam String purpose, @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.TIME, pattern="yyyy-mm-dd") date, ) throws Exception{
-    
-//   }
+    // @PutMapping
+    // public IdeaDTO modifyIdea(@RequestParam String title, @RequestParam String
+    // purpose, @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.TIME,
+    // pattern="yyyy-mm-dd") date, ) throws Exception{
+
+    // }
+
+    @GetMapping("/{id}/details")
+    public DetailedIdeaDTO getIdeaDetailsById(@PathVariable String id) {
+        Idea idea = ideaService.getIdeaById(id);
+        return new DetailedIdeaDTO(idea);
+    }
 }
