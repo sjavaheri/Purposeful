186b140d0f5f86b12c96f0d1ab3c24b46b63f160
Wassim Jabbour <wassim.jabbour@mail.mcgill.ca>
2023-02-19 20:54:58 -0500

    Finalize controller, refactor DTOs

--- backend/src/main/java/ca/mcgill/purposeful/controller/IdeaController.java
+++ backend/src/main/java/ca/mcgill/purposeful/controller/IdeaController.java
@@ -3,31 +3,46 @@ package ca.mcgill.purposeful.controller;
 import ca.mcgill.purposeful.dto.IdeaDTO;
 import ca.mcgill.purposeful.model.Idea;
 import ca.mcgill.purposeful.service.IdeaService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /** API for demonstrating how permissions work for access to endpoints */
 @RestController
-@RequestMapping({"/idea", "/idea/"})
+@RequestMapping({"api/idea", "api/idea/"})
 public class IdeaController {
   @Autowired IdeaService ideaService;
 
-  @GetMapping("/api/idea/{id}")
+  @GetMapping("{id}")
+  @PreAuthorize("hasAnyAuthority('User', 'Moderator', 'Owner')")
   public ResponseEntity<IdeaDTO> getIdeaById(@PathVariable String id) {
     Idea idea = ideaService.getIdeaById(id);
     return ResponseEntity.status(HttpStatus.OK).body(new IdeaDTO(idea));
   }
 
-  //  @GetMapping("/api/idea")
-  //  public List<IdeaDTO> filterIdeas(
-  //      @RequestParam(value = "topics") String[] topics,
-  //      @RequestParam(value = "domains") String[] domains,
-  //      @RequestParam(value = "techs") String[] techs) {
-  //    return ideaService.(topics, domains, techs);;
-  //  }
+  /**
+   * Filter ideas by topics, domains, and techs. Results are ordered by date.
+   *
+   * @param topics <></>he topics to filter by (optional so that it can be null if no filter)
+   * @param domains The domains to filter by (optional so that it can be null if no filter)
+   * @param techs The techs to filter by (optional so that it can be null if no filter)
+   * @return A list of idea DTOs that matches the filters
+   * @author Wassim Jabbour
+   */
+  @GetMapping
+  @PreAuthorize("hasAnyAuthority('User', 'Moderator', 'Owner')")
+  public ResponseEntity<List<IdeaDTO>> filterIdeas(
+      @RequestParam(value = "topics", required = false) List<String> topics,
+      @RequestParam(value = "domains", required = false) List<String> domains,
+      @RequestParam(value = "techs", required = false) List<String> techs) {
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(IdeaDTO.convertToDto(ideaService.getIdeasByAllCriteria(topics, domains, techs)));
+  }
 }
