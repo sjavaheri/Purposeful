034274b568415e7846baaab31f931396e1a6cff7
Wassim Jabbour <wassim.jabbour@mail.mcgill.ca>
2023-02-19 20:03:23 -0500

    Refactor some of Ramin's changes

--- backend/src/main/java/ca/mcgill/purposeful/controller/IdeaController.java
+++ backend/src/main/java/ca/mcgill/purposeful/controller/IdeaController.java
@@ -1,47 +1,33 @@
 package ca.mcgill.purposeful.controller;
 
+import ca.mcgill.purposeful.dto.IdeaDTO;
+import ca.mcgill.purposeful.model.Idea;
+import ca.mcgill.purposeful.service.IdeaService;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.format.annotation.DateTimeFormat;
 
-
-import ca.mcgill.purposeful.dto.DetailedIdeaDTO;
-import ca.mcgill.purposeful.model.Idea;
-import ca.mcgill.purposeful.service.IdeaService;
-
-
-/**
- * API for demonstrating how permissions work for access to endpoints
- */
+/** API for demonstrating how permissions work for access to endpoints */
 @RestController
-@RequestMapping({ "/idea", "/idea/" })
-
+@RequestMapping({"/idea", "/idea/"})
 public class IdeaController {
-    @Autowired
-    IdeaService ideaService;
+  @Autowired IdeaService ideaService;
 
-    /**
-     * This methods gets all librarians for the database
-     * 
-     * @author Ramin Akhavan
-     * @return update idea
-     * @throws Exception
-     */
-    // @PutMapping
-    // public IdeaDTO modifyIdea(@RequestParam String title, @RequestParam String
-    // purpose, @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.TIME,
-    // pattern="yyyy-mm-dd") date, ) throws Exception{
+  @GetMapping("/api/idea/{id}")
+  public ResponseEntity<IdeaDTO> getIdeaById(@PathVariable String id) {
+    Idea idea = ideaService.getIdeaById(id);
+    return ResponseEntity.status(HttpStatus.OK).body(new IdeaDTO(idea));
+  }
 
-    // }
-
-    @GetMapping("/{id}/details")
-    public DetailedIdeaDTO getIdeaDetailsById(@PathVariable String id) {
-        Idea idea = ideaService.getIdeaById(id);
-        return new DetailedIdeaDTO(idea);
-    }
-
+  //  @GetMapping("/api/idea")
+  //  public List<IdeaDTO> filterIdeas(
+  //      @RequestParam(value = "topics") String[] topics,
+  //      @RequestParam(value = "domains") String[] domains,
+  //      @RequestParam(value = "techs") String[] techs) {
+  //    return ideaService.(topics, domains, techs);;
+  //  }
 }
