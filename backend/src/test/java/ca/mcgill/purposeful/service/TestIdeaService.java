9edfab3365b0dd579001ee0013e97c5650caba1b
rakhavan55 <ramin.akhavan@mail.mcgill.ca>
2023-02-19 21:52:19 -0500

    Added modifying idea tests, using Wassim's mock repo

--- backend/src/test/java/ca/mcgill/purposeful/service/TestIdeaService.java
+++ backend/src/test/java/ca/mcgill/purposeful/service/TestIdeaService.java
@@ -9,23 +9,31 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.lenient;
 
+import ca.mcgill.purposeful.dao.DomainRepository;
 import ca.mcgill.purposeful.dao.IdeaRepository;
+import ca.mcgill.purposeful.dao.TechnologyRepository;
+import ca.mcgill.purposeful.dao.TopicRepository;
+import ca.mcgill.purposeful.dao.URLRepository;
 import ca.mcgill.purposeful.model.Domain;
 import ca.mcgill.purposeful.model.Idea;
 import ca.mcgill.purposeful.model.RegularUser;
 import ca.mcgill.purposeful.model.Technology;
 import ca.mcgill.purposeful.model.Topic;
+import ca.mcgill.purposeful.model.URL;
+import io.cucumber.java.hu.Ha;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -37,11 +45,26 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 public class TestIdeaService {
 
+  private static String NEW_TITLE = "new title";
+  private static String NEW_PURPOSE = "new purpose";
+  private static String NEW_DESCRIPTION = "new description";
+
+  private static boolean NEW_PAY = true;
+  private static boolean NEW_PRIVACY = true;
+  private static boolean NEW_PROGRESS = true;
+
+  private static Date NEW_DATE = new Date(16000);
+
   // Mocks
   @Mock private IdeaRepository ideaRepository;
+  @Mock private DomainRepository domainRepository;
+  @Mock private TopicRepository topicRepository;
+  @Mock private TechnologyRepository technologyRepository;
+  @Mock private URLRepository urlRepository;
 
   // Inject mocks
   @InjectMocks private IdeaService ideaService;
+
 
   // Set the mock output of each function in the repository
   @BeforeEach
@@ -51,6 +74,18 @@ public class TestIdeaService {
         .thenAnswer(MockRepository::findIdeaById);
     lenient().when(ideaRepository.save(any(Idea.class))).thenAnswer(MockRepository::save);
     lenient().when(ideaRepository.findAll()).thenAnswer(MockRepository::findAll);
+    lenient()
+        .when(domainRepository.findDomainById(anyString()))
+        .thenAnswer(MockRepository::findDomainById);
+    lenient()
+        .when(topicRepository.findTopicById(anyString()))
+        .thenAnswer(MockRepository::findTopicById);
+    lenient()
+        .when(technologyRepository.findTechnologyById(anyString()))
+        .thenAnswer(MockRepository::findTechnologyById);
+    lenient()
+        .when(urlRepository.findURLById(anyString()))
+        .thenAnswer(MockRepository::findURLById);
   }
 
   /**
@@ -209,6 +244,121 @@ public class TestIdeaService {
     fail();
   }
 
+  /** 
+   * @author Ramin Akhavan
+   * Test all attributes changing 
+   */
+  @Test
+  public void testModifyAllAttributesOfIdea() {
+    List<String> domainIds = new ArrayList<String>();
+    List<String> topicIds = new ArrayList<String>();
+    List<String> techIds = new ArrayList<String>();
+    List<String> imgUrlIds = new ArrayList<String>();
+
+    // Retrieve Ids of all objects
+    for (Domain domain: MockDatabase.modifiableDomainGroup){
+        domainIds.add(domain.getId());
+    }
+    for (Topic topic: MockDatabase.modifiableTopicGroup){
+        topicIds.add(topic.getId());
+    }
+    for (Technology tech: MockDatabase.modifiableTechGroup){
+        techIds.add(tech.getId());
+    }
+    for (URL url: MockDatabase.modifiableImgUrlGroup){
+        imgUrlIds.add(url.getId());
+    }
+    // Modify all attributes of idea
+    Idea updatedIdea = null;
+    try{
+        updatedIdea = ideaService.modifyIdea(MockDatabase.modifiableIdea.getId(), NEW_TITLE, NEW_DATE, NEW_PURPOSE, NEW_DESCRIPTION, NEW_PAY, NEW_PROGRESS, NEW_PRIVACY, domainIds, techIds, topicIds, imgUrlIds, MockDatabase.newIconUrl.getId());
+    } catch (Exception e) {
+        String message = e.getMessage();
+    }
+
+    // Test all attributes of idea
+    assertEquals(NEW_TITLE, updatedIdea.getTitle());
+    assertEquals(NEW_PURPOSE, updatedIdea.getPurpose());
+    assertEquals(NEW_DESCRIPTION, updatedIdea.getDescription());
+
+    assertEquals(NEW_PAY, updatedIdea.isPaid());
+    assertEquals(NEW_PROGRESS, updatedIdea.isInProgress());
+    assertEquals(NEW_PRIVACY, updatedIdea.isPrivate());
+
+    assertEquals(NEW_DATE.toString(), updatedIdea.getDate().toString());
+
+    // Check Ids of all objects of the idea
+    for(Domain domain: updatedIdea.getDomains()){
+        assertTrue(domainIds.contains(domain.getId()));
+    }
+    for(Technology tech: updatedIdea.getTechs()){
+        assertTrue(techIds.contains(tech.getId()));
+    }
+    for(Topic topic: updatedIdea.getTopics()){
+        assertTrue(topicIds.contains(topic.getId()));
+    }
+    for(URL url: updatedIdea.getSupportingImageUrls()){
+        assertTrue(imgUrlIds.contains(url.getId()));
+    }
+    assertEquals(updatedIdea.getIconUrl().getId(), MockDatabase.newIconUrl.getId());
+
+  }
+
+  /** 
+   * @author Ramin Akhavan
+   * Test empty attribute violation 
+   */
+  @Test
+  public void testModifyIdeaWithInvalidEmptyFieldFailure() {
+    List<String> domainIds = new ArrayList<String>();
+    List<String> topicIds = new ArrayList<String>();
+    List<String> techIds = new ArrayList<String>();
+    List<String> imgUrlIds = new ArrayList<String>();
+
+    // Modify an existing idea with an empty title
+    Idea updatedIdea = null;
+    String message = "";
+    try{
+        updatedIdea = ideaService.modifyIdea(MockDatabase.modifiableIdea.getId(), "", NEW_DATE, NEW_PURPOSE, NEW_DESCRIPTION, NEW_PAY, NEW_PROGRESS, NEW_PRIVACY, domainIds, techIds, topicIds, imgUrlIds, MockDatabase.newIconUrl.getId());
+    } catch (Exception e) {
+        message = e.getMessage();
+    }
+
+    // Check error message
+    assertEquals("Necessary fields have been left empty", message);
+
+  }
+
+  /** 
+   * @author Ramin Akhavan
+   * Test non-exsting object violation
+   */
+  @Test
+  public void testModifyIdeaWithNonExistingObjectFailure() {
+    List<String> domainIds = new ArrayList<String>();
+    List<String> topicIds = new ArrayList<String>();
+    List<String> techIds = new ArrayList<String>();
+    List<String> imgUrlIds = new ArrayList<String>();
+
+    
+    String fakeId = "Ram";
+    domainIds.add(fakeId);
+
+    // Modify an idea with a non-existing object
+    Idea updatedIdea = null;
+    String message = "";
+    try{
+        updatedIdea = ideaService.modifyIdea(MockDatabase.modifiableIdea.getId(), NEW_TITLE, NEW_DATE, NEW_PURPOSE, NEW_DESCRIPTION, NEW_PAY, NEW_PROGRESS, NEW_PRIVACY, domainIds, techIds, topicIds, imgUrlIds, MockDatabase.newIconUrl.getId());
+    } catch (Exception e) {
+        message = e.getMessage();
+    }
+    // Check error message
+    assertEquals("You are attempting to link your idea to an object that does not exist", message);
+
+  }
+
+
+
   /**
    * This class holds all of the mock methods of the CRUD repositories
    *
@@ -221,28 +371,91 @@ public class TestIdeaService {
         return MockDatabase.idea1;
       } else if (id.equals(MockDatabase.idea2.getId())) {
         return MockDatabase.idea2;
+      } else if (id.equals(MockDatabase.modifiableIdea.getId())) {
+        return MockDatabase.modifiableIdea;
       } else {
         return null;
       }
     }
 
+    static Domain findDomainById(InvocationOnMock invocation) {
+        String id = invocation.getArgument(0);
+        if (id.equals(MockDatabase.originalDomain.getId())) {
+          return MockDatabase.originalDomain;
+        } else if (id.equals(MockDatabase.newDomain.getId())) {
+          return MockDatabase.newDomain;
+        } else if (id.equals(MockDatabase.domain1.getId())) {
+          return MockDatabase.domain1;
+        } else if (id.equals(MockDatabase.domain2.getId())) {
+            return MockDatabase.domain2;
+        } else {
+          return null;
+        }
+    }
+
+    static Technology findTechnologyById(InvocationOnMock invocation) {
+        String id = invocation.getArgument(0);
+        if (id.equals(MockDatabase.originalTech.getId())) {
+          return MockDatabase.originalTech;
+        } else if (id.equals(MockDatabase.newTech.getId())) {
+          return MockDatabase.newTech;
+        } else if (id.equals(MockDatabase.tech1.getId())) {
+          return MockDatabase.tech1;
+        } else if (id.equals(MockDatabase.tech2.getId())) {
+            return MockDatabase.tech2;
+        } else {
+          return null;
+        }
+    }
+
+    static Topic findTopicById(InvocationOnMock invocation) {
+        String id = invocation.getArgument(0);
+        if (id.equals(MockDatabase.originalTopic.getId())) {
+          return MockDatabase.originalTopic;
+        } else if (id.equals(MockDatabase.newTopic.getId())) {
+          return MockDatabase.newTopic;
+        } else if (id.equals(MockDatabase.topic1.getId())) {
+          return MockDatabase.topic1;
+        } else if (id.equals(MockDatabase.topic2.getId())) {
+            return MockDatabase.topic2;
+        } else if (id.equals(MockDatabase.topic3.getId())) {
+            return MockDatabase.topic3;
+        } else {
+          return null;
+        }
+    }
+
+    static URL findURLById(InvocationOnMock invocation) {
+        String id = invocation.getArgument(0);
+        if (id.equals(MockDatabase.originalImgUrl.getId())) {
+          return MockDatabase.originalImgUrl;
+        } else if (id.equals(MockDatabase.newImgUrl.getId())) {
+          return MockDatabase.newImgUrl;
+        } else if (id.equals(MockDatabase.newImgUrl2.getId())) {
+          return MockDatabase.newImgUrl2;
+        } else if (id.equals(MockDatabase.newIconUrl.getId())) {
+            return MockDatabase.newIconUrl;
+        } else if (id.equals(MockDatabase.originalIconUrl.getId())) {
+            return MockDatabase.originalIconUrl;
+        } else {
+          return null;
+        }
+    }
+
+
     static Idea save(InvocationOnMock invocation) {
       return invocation.getArgument(0);
     }
 
-    static Iterable<Idea> findAll(InvocationOnMock invocation) {
+    static Iterator<Idea> findAll(InvocationOnMock invocation) {
       HashSet<Idea> ideas = new HashSet<>();
       ideas.add(MockDatabase.idea1);
       ideas.add(MockDatabase.idea2);
-      return ideas;
+      return ideas.iterator();
     }
   }
 
-  /**
-   * This class holds all of the mock objects of the database *
-   *
-   * @author Wassim Jabbour
-   */
+  /** This class holds all of the mock objects of the database */
   static final class MockDatabase {
 
     /** Create mock objects here * */
@@ -252,6 +465,8 @@ public class TestIdeaService {
 
     static Idea idea2 = new Idea();
 
+    static Idea modifiableIdea = new Idea();
+
     // Users
     static RegularUser user1 = new RegularUser();
 
@@ -259,9 +474,15 @@ public class TestIdeaService {
     static Domain domain1 = new Domain();
     static Domain domain2 = new Domain();
 
+    static Domain originalDomain = new Domain();
+    static Domain newDomain = new Domain();
+
     // Domain groups (A set of multiple of the above domains)
     static HashSet<Domain> domainGroup1 = new HashSet<>();
     static HashSet<Domain> domainGroup2 = new HashSet<>();
+
+    static HashSet<Domain> originalDomainGroup = new HashSet<>();
+    static HashSet<Domain> modifiableDomainGroup = new HashSet<>();
 
     // Topics
     static Topic topic1 = new Topic();
@@ -269,18 +490,42 @@ public class TestIdeaService {
     static Topic topic3 = new Topic();
     static Topic topic4 = new Topic();
 
+    static Topic originalTopic = new Topic();
+    static Topic newTopic = new Topic();
+
     // Topic groups (A set of multiple of the above topics)
     static HashSet<Topic> topicGroup1 = new HashSet<>();
     static HashSet<Topic> topicGroup2 = new HashSet<>();
     static HashSet<Topic> topicGroup3 = new HashSet<>();
 
+    static HashSet<Topic> originalTopicGroup = new HashSet<>();
+    static HashSet<Topic> modifiableTopicGroup = new HashSet<>();
+
     // Techs
     static Technology tech1 = new Technology();
     static Technology tech2 = new Technology();
 
+    static Technology originalTech = new Technology();
+    static Technology newTech = new Technology();
+
     // Tech groups (A set of multiple of the above techs)
     static HashSet<Technology> techGroup1 = new HashSet<>();
     static HashSet<Technology> techGroup2 = new HashSet<>();
+
+    static HashSet<Technology> originalTechGroup = new HashSet<>();
+    static HashSet<Technology> modifiableTechGroup = new HashSet<>();
+
+
+    static URL originalImgUrl = new URL();
+    static URL newImgUrl = new URL();
+    static URL newImgUrl2 = new URL();
+
+
+    static List<URL> originalImgUrlGroup = new ArrayList<>();
+    static List<URL> modifiableImgUrlGroup = new ArrayList<>();
+
+    static URL originalIconUrl = new URL();
+    static URL newIconUrl = new URL();
 
     /**
      * Initialize fields here
@@ -301,6 +546,12 @@ public class TestIdeaService {
 
       topic4.setId(UUID.randomUUID().toString());
       topic4.setName("Business");
+    
+      originalTopic.setId(UUID.randomUUID().toString());
+      originalTopic.setName("Algebra");
+
+      newTopic.setId(UUID.randomUUID().toString());
+      newTopic.setName("Calculus");
 
       // Initialize topic groups by merging topics
       topicGroup1.add(topic1);
@@ -310,6 +561,9 @@ public class TestIdeaService {
       topicGroup2.add(topic1);
 
       topicGroup3.add(topic4);
+      originalTopicGroup.add(originalTopic);
+      modifiableTopicGroup.add(originalTopic);
+      modifiableTopicGroup.add(newTopic);
 
       // Initialize domains
       domain1.setId(UUID.randomUUID().toString());
@@ -318,11 +572,21 @@ public class TestIdeaService {
       domain2.setId(UUID.randomUUID().toString());
       domain2.setName("business");
 
+      originalDomain.setId(UUID.randomUUID().toString());
+      originalDomain.setName("Management");
+
+      newDomain.setId(UUID.randomUUID().toString());
+      newDomain.setName("Photography");
+
       // Initialize domain groups by merging domains
       domainGroup1.add(domain1);
       domainGroup1.add(domain2);
 
       domainGroup2.add(domain1);
+
+      originalDomainGroup.add(originalDomain);
+      modifiableDomainGroup.add(originalDomain);
+      modifiableDomainGroup.add(newDomain);
 
       // Initialize techs
       tech1.setId(UUID.randomUUID().toString());
@@ -331,11 +595,40 @@ public class TestIdeaService {
       tech2.setId(UUID.randomUUID().toString());
       tech2.setName("Python");
 
+      originalTech.setId(UUID.randomUUID().toString());
+      originalTech.setName("Pascal");
+
+      newTech.setId(UUID.randomUUID().toString());
+      newTech.setName("Cobol");
+
       // Initialize tech groups by merging techs
       techGroup1.add(tech1);
       techGroup1.add(tech2);
 
       techGroup2.add(tech1);
+
+      originalTechGroup.add(originalTech);
+      modifiableTechGroup.add(newTech);
+
+      originalImgUrl.setId(UUID.randomUUID().toString());
+      originalImgUrl.setURL("www.something.com");
+
+      newImgUrl.setId(UUID.randomUUID().toString());
+      newImgUrl.setURL("www.another.com");
+
+      newImgUrl2.setId(UUID.randomUUID().toString());
+      newImgUrl2.setURL("www.thirdone.com");
+      
+      originalImgUrlGroup.add(originalImgUrl);
+      modifiableImgUrlGroup.add(newImgUrl);
+      modifiableImgUrlGroup.add(newImgUrl2);
+    
+      originalIconUrl.setId(UUID.randomUUID().toString());
+      originalIconUrl.setURL("www.mainicon.com");
+
+      newIconUrl.setId(UUID.randomUUID().toString());
+      newIconUrl.setURL("www.newmainicon.com");
+
 
       // Initialize ideas
       idea1.setId(UUID.randomUUID().toString());
@@ -355,6 +648,18 @@ public class TestIdeaService {
       idea2.setTopics(topicGroup2);
       idea2.setTechs(techGroup2);
       idea2.setUser(user1);
+
+      modifiableIdea.setId(UUID.randomUUID().toString());
+      modifiableIdea.setDate(
+          new Date(14000)); // 10000 seconds since 1970 (Other constructors are deprecated)
+      modifiableIdea.setPaid(false);
+      modifiableIdea.setPrivate(false);
+      modifiableIdea.setInProgress(false);;
+      modifiableIdea.setDescription("Volatile application");
+      modifiableIdea.setDomains(originalDomainGroup);
+      modifiableIdea.setTopics(originalTopicGroup);
+      modifiableIdea.setTechs(originalTechGroup);
+      modifiableIdea.setUser(user1);
     }
   }
 }
