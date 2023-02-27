f87ad2e4cee4fb552b2627d06b4b0137b999564c
Siger Ma <--local>
2023-02-26 20:45:38 -0500

    Remove temporary bootrun malfunction

--- backend/src/main/java/ca/mcgill/purposeful/PurposefulApplication.java
+++ backend/src/main/java/ca/mcgill/purposeful/PurposefulApplication.java
@@ -1,13 +1,7 @@
 package ca.mcgill.purposeful;
 
-import ca.mcgill.purposeful.configuration.Authority;
-import ca.mcgill.purposeful.dao.AppUserRepository;
-import ca.mcgill.purposeful.model.AppUser;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 /** Main Application */
 @SpringBootApplication
@@ -24,47 +18,47 @@ public class PurposefulApplication {
    * @param passwordEncoder to encode the user's password
    * @return
    */
-  @Bean
-  public CommandLineRunner commandLineRunner(
-      AppUserRepository appUserRepository, PasswordEncoder passwordEncoder) {
-    return args -> {
-
-      // create an owner at bootstrap
-      if (appUserRepository.findAppUserByEmail("owner@gmail.com") == null) {
-        AppUser appUser = new AppUser();
-        appUser.setFirstname("Big");
-        appUser.setLastname("Boss");
-        appUser.setEmail("owner@email.com");
-        // encode the password
-        String encoded = passwordEncoder.encode("owner");
-        appUser.setPassword(encoded);
-        appUser.getAuthorities().add(Authority.Owner);
-        appUserRepository.save(appUser);
-      }
-      if (appUserRepository.findAppUserByEmail("moderator@email.com") == null) {
-        // create a moderator at bootstrap
-        AppUser appUser1 = new AppUser();
-        appUser1.setFirstname("Marwan");
-        appUser1.setLastname("Kanaan");
-        appUser1.setEmail("moderator@email.com");
-        // encode the password
-        String encoded1 = passwordEncoder.encode("moderator");
-        appUser1.setPassword(encoded1);
-        appUser1.getAuthorities().add(Authority.Moderator);
-        appUserRepository.save(appUser1);
-      }
-      // create a user at bootstrap
-      if (appUserRepository.findAppUserByEmail("user@email.com") == null) {
-        AppUser appUser2 = new AppUser();
-        appUser2.setFirstname("Rob");
-        appUser2.setLastname("Sab");
-        appUser2.setEmail("user@email.com");
-        // encode the password
-        String encoded2 = passwordEncoder.encode("user");
-        appUser2.setPassword(encoded2);
-        appUser2.getAuthorities().add(Authority.User);
-        appUserRepository.save(appUser2);
-      }
-    };
-  }
+  //  @Bean
+  //  public CommandLineRunner commandLineRunner(
+  //      AppUserRepository appUserRepository, PasswordEncoder passwordEncoder) {
+  //    return args -> {
+  //
+  //      // create an owner at bootstrap
+  //      if (appUserRepository.findAppUserByEmail("owner@gmail.com") == null) {
+  //        AppUser appUser = new AppUser();
+  //        appUser.setFirstname("Big");
+  //        appUser.setLastname("Boss");
+  //        appUser.setEmail("owner@email.com");
+  //        // encode the password
+  //        String encoded = passwordEncoder.encode("owner");
+  //        appUser.setPassword(encoded);
+  //        appUser.getAuthorities().add(Authority.Owner);
+  //        appUserRepository.save(appUser);
+  //      }
+  //      if (appUserRepository.findAppUserByEmail("moderator@email.com") == null) {
+  //        // create a moderator at bootstrap
+  //        AppUser appUser1 = new AppUser();
+  //        appUser1.setFirstname("Marwan");
+  //        appUser1.setLastname("Kanaan");
+  //        appUser1.setEmail("moderator@email.com");
+  //        // encode the password
+  //        String encoded1 = passwordEncoder.encode("moderator");
+  //        appUser1.setPassword(encoded1);
+  //        appUser1.getAuthorities().add(Authority.Moderator);
+  //        appUserRepository.save(appUser1);
+  //      }
+  //      // create a user at bootstrap
+  //      if (appUserRepository.findAppUserByEmail("user@email.com") == null) {
+  //        AppUser appUser2 = new AppUser();
+  //        appUser2.setFirstname("Rob");
+  //        appUser2.setLastname("Sab");
+  //        appUser2.setEmail("user@email.com");
+  //        // encode the password
+  //        String encoded2 = passwordEncoder.encode("user");
+  //        appUser2.setPassword(encoded2);
+  //        appUser2.getAuthorities().add(Authority.User);
+  //        appUserRepository.save(appUser2);
+  //      }
+  //    };
+  //  }
 }
