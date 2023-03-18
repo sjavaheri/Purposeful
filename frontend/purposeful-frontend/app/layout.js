017ad033a17317f0dfbd41245f375b115140b428
Siger Ma <--local>
2023-03-18 16:51:07 -0400

    T186 optimize imports

--- frontend/purposeful-frontend/app/layout.js
+++ frontend/purposeful-frontend/app/layout.js
@@ -1,8 +1,7 @@
 "use client";
 
 import { CacheProvider } from "@chakra-ui/next-js";
-import { ChakraProvider } from "@chakra-ui/react";
-import { ColorModeScript } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./theme";
 
 export default function RootLayout({ children }) {
