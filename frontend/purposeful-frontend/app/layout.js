bb3e75b74c62ea17e7119702dde4b316364f311e
Siger Ma <--local>
2023-03-18 15:44:33 -0400

    T173 Added theming for Chakra UI

--- frontend/purposeful-frontend/app/layout.js
+++ frontend/purposeful-frontend/app/layout.js
@@ -2,12 +2,15 @@
 
 import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraProvider } from "@chakra-ui/react";
+import { ColorModeScript } from "@chakra-ui/react";
+import theme from "./theme";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head />
       <body>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <CacheProvider>
           <ChakraProvider>{children}</ChakraProvider>
         </CacheProvider>
