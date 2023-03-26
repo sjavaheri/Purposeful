f2ec66dfc0f260fe5c815eddcd4194c503c19417
Thibaut Baguette <thibaut.baguette@mail.mcgill.ca>
2023-03-26 16:09:19 -0400

    Make component fetch data

--- frontend/purposeful-frontend/components/CollaborationResponse.js
+++ frontend/purposeful-frontend/components/CollaborationResponse.js
@@ -2,25 +2,58 @@ import {
     Popover, PopoverTrigger, PopoverContent, PopoverArrow, PopoverCloseButton, PopoverHeader, PopoverBody,
     Button, Badge
 } from '@chakra-ui/react';
+import fetchWrapper from '@/utils/fetch_wrapper';
+import { useEffect, useState } from 'react';
 
-export default function CollaborationResponse({ collaborationResponse }) {
-    // const message = collaborationResponse.message;
-    // var additionalContact = collaborationResponse.additionalContact;
-    const message = "I would love to work with you!";
-    var additionalContact = "123-456-7890";
+export default function CollaborationResponse({ ideaId }) {
+    // User and authentication info
+    // const [GrantedAuth, setGrantedAuth] = useState([]);
+    // useEffect(() => {
+    //     setGrantedAuth(getAuthorities());
+    // }, []);
 
+    const [collaborationResponse, setCollaborationResponse] = useState({});
+
+    useEffect(() => {
+        const fetchCollaborationResponse = async () => {
+            const response = await fetchWrapper('/api/collaborationResponse/' + ideaId)
+
+            const handleData = async (response) => {
+                if (!response.ok) {
+                    return null;
+                } else {
+                    return await response.json();
+                }
+            }
+
+            setCollaborationResponse(await handleData(response));
+        };
+        fetchCollaborationResponse();
+    }, [ideaId]);
+
+    if (collaborationResponse === null) {
+        return null;
+    }
+
+    var message = collaborationResponse.message;
+    message = (
+        <div>
+            <b>Message:</b> {message}
+        </div>
+    )
+
+    var additionalContact = collaborationResponse.additionalContact;
     if (additionalContact === "") {
         additionalContact = null;
     } else {
         additionalContact = (
             <div>
-                <b>Additional Contact:</b> {additionalContact}
+                <b>Contact Information:</b> {additionalContact}
             </div>
         )
     }
 
-    // var status = collaborationResponse.status;
-    var status = "Approved";
+    var status = collaborationResponse.status;
 
     if (status === "Approved") {
         status = (
@@ -44,8 +77,8 @@ export default function CollaborationResponse({ collaborationResponse }) {
                 <PopoverCloseButton />
                 <PopoverHeader>Response {status}</PopoverHeader>
                 <PopoverBody>
-                    {message} <br />
-                    {additionalContact}
+                    {additionalContact} <br />
+                    {message}
                 </PopoverBody>
             </PopoverContent>
         </Popover>
