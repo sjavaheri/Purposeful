89883123763a4b34ce192bb37c48add6eea3c7c0
rakhavan55 <ramin.akhavan@mail.mcgill.ca>
2023-03-26 01:52:00 -0400

    Initial modify idea components complete, consistent and similar to create idea

---
+++ frontend/purposeful-frontend/components/ContainerLabel.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Box, HStack, IconButton} from '@chakra-ui/react';
+import { RxCross1} from "react-icons/rx";
+
+/**
+ * 
+ * @author Adam Kazma
+ */
+export default function ContainerLabel({innerTxt, arr, arr2, refresh}){
+    function find_component_in_arr(c_arr){
+        for(var i = 0; i < c_arr.length; i++){
+            if((c_arr[i].name) === innerTxt){
+                return i;
+            }
+        }
+        return -1;
+    }
+    return (
+        <HStack fontSize={"small"} paddingLeft={2} borderRadius={"10px"} borderStyle={"solid"} border={"solid"} borderColor={"rgba(255,255,255,0.6)"} width={"fit-content"} backgroundColor={"rgba(0,0,0,0.2)"}>
+        <span>{innerTxt}</span>
+        <IconButton
+            size='sm'
+            icon={<RxCross1/>}
+            onClick = {function(){
+                var indexw = find_component_in_arr(arr);
+                arr.splice(indexw,1);
+                arr2.splice(indexw,1);
+                refresh();
+                //ReactDOM.unmountComponentAtNode(ReactDOM.findDOMNode(this).parentNode);
+            }}
+        />
+        </HStack>
+    );
+}
