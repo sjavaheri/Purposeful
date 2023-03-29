149e48e0bf32d1392bef93ed2a47b3251096b9b9
Adam Kazma <adkazma2@gmail.com>
2023-03-28 22:53:40 -0400

    Collab Request frontend final touches

--- frontend/purposeful-frontend/components/CollabRequestPopup.js
+++ frontend/purposeful-frontend/components/CollabRequestPopup.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import {Box, Text, Button, Stack, Flex, IconButton, Textarea} from '@chakra-ui/react';
+import {Input, Box, Text, Button, Stack, Flex, IconButton, Textarea} from '@chakra-ui/react';
 import { RxCross1} from "react-icons/rx";
 
 
-export default function CollabRequestPopup({RequesterMail,ideaID,ideaTitle,removeFunc}){
-    var reqMessage = "";
+export default function CollabRequestPopup({RequesterMail,ideaID,ideaTitle,removeFunc,disp}){
     return (
-        <Stack width={"40%"} alignSelf={"center"} marginTop={"10%"} position={"absolute"} borderRadius={"10px"} backgroundColor={"rgba(20,20,20,0.05)"}>
+        <Stack zIndex={"1"} marginTop={"5%"} marginLeft={"30%"} borderColor={"rgba(0,0,0,0.1)"} border={"solid"} width={"40%"} alignSelf={"center"} position={"fixed"} borderRadius={"10px"} backgroundColor={"rgba(255,255,255,1)"}>
             <Box width={"100%"}>
-                <Flex width={"100%"} backgroundColor={"rgba(10,10,10,0.1)"} borderRadius={"10px"}>
+                <Flex width={"100%"} backgroundColor={"rgba(10,10,10,0.1)"} borderRadius={"10px"} borderBottom={"solid"}>
                 <Text marginLeft={"10px"}>Collaboration Request</Text>
                 <IconButton
+                borderLeft={"solid"}
+                borderRight={"solid"}
                 marginRight={"0"}
                 marginLeft={"auto"}
                 size={'sm'}
                 icon={<RxCross1/>}
+                onClick={() => removeFunc()}
                 />
                 </Flex>
             </Box>
@@ -22,7 +24,11 @@ export default function CollabRequestPopup({RequesterMail,ideaID,ideaTitle,remov
                 <Text marginLeft={"0"} marginRight={"auto"}>User Email: {RequesterMail}</Text>
                 <Text marginLeft={"auto"} marginRight={"0"}>Idea: {ideaTitle}</Text>
             </Flex>
-            <Textarea alignSelf={"center"} width={"90%"} placeholder={"Add a custom collaboration message..."}>{reqMessage}</Textarea>
+            <Textarea id={"reqMessage"} alignSelf={"center"} width={"90%"} placeholder={"Add a collaboration invitation message..."}></Textarea>
+            <Flex padding={1} width={"80%"} alignSelf={"center"}>
+                <Text>Additional Contact :</Text>
+                <Input id={"addContact"} placeholder="Additional Contact..." type="text"/>
+            </Flex>
             <Button
             bg={"blue.400"}
             color={"white"}
@@ -34,11 +40,12 @@ export default function CollabRequestPopup({RequesterMail,ideaID,ideaTitle,remov
             alignSelf={"center"}
             iconRight={<RxCross1/>}
             onClick = {function(){
-                
+                //Connect to backend here
             }}
             >
                 Send Collab Request
             </Button>
+            <br></br>
         </Stack>
     );
 }
